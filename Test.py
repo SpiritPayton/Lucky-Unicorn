f30b7cb38bef67939e0868b94979658dabe3de9f
Spirit Payton <98915494+SpiritPayton@users.noreply.github.com>
2022-04-05 14:29:01 +1200

    Update Test.py

--- Test.py
+++ Test.py
@@ -3,20 +3,22 @@ Simplifies the input by converting it to lower case. Also, accepts y or n as
 abbreviations. Prints result of user choice as well as input - for testing.
 """
 
+show_instructions = ""
+while show_instructions != "x":
 
-# Ask user if they have played the game before
-show_instructions = input("Have you played this game before?: ").lower()
+    # Ask user if they have played the game before
+    show_instructions = input("Have you played this game before?: ").lower()
 
-# If they say yes, output "Program Continues"
-if show_instructions == "yes" or show_instructions == "y":
-    print("Program continues")
+    # If they say yes, output "Program Continues"
+    if show_instructions == "yes" or show_instructions == "y":
+        print("Program continues")
 
-# If they say no, output "Display Instructions"
-elif show_instructions == "no" or show_instructions == "n":
-    print("Display instructions")
+    # If they say no, output "Display Instructions"
+    elif show_instructions == "no" or show_instructions == "n":
+        print("Display instructions")
 
-# Otherwise, show error
-else:
-    print("Please enter 'yes' or 'no'.")
+    # Otherwise, show error
+    else:
+        print("Please enter 'yes' or 'no'.")
 
-print(f"You entered '{show_instructions}'.")
+    print(f"You entered '{show_instructions}'.")
