05eb64d25cd714c2796bcaa9e353d1b33bf0e386
Spirit Payton <98915494+SpiritPayton@users.noreply.github.com>
2022-04-13 10:30:18 +1200

    Update 06 rounds v1.py

--- 06 rounds v1.py
+++ 06 rounds v1.py
@@ -11,3 +11,40 @@ TEST_AMOUNT = 5
 balance = TEST_AMOUNT
 
 rounds_played = 0
+play_again = ""
+
+# testing loop to generate 5 tokens
+while play_again != "x":
+    rounds_played += 1 # keep track of rounds
+    number = random.randint(6, 36)  # can only be a donkey
+
+    # adjust balance
+    # if the random number is between 1 and 5
+    # user gets a unicorn (add $4 to balance)
+    if 1 <= number <= 5:
+        token = "unicorn"
+        balance += 4
+
+    # if the random number is between 6 and 36
+    # user gets a donkey (subtract $1 from balance)
+    elif 6 <= <= 36:
+        token = "donkey"
+        balance -= 1
+
+    # in all other cases the token must be a horse or a zebra
+    # (subtract $0.50 from the balance in either case)
+    else:
+        # if the number is even, set the token to zebra
+        if number % 2 == 0:
+            token = "zebra"
+            balance -= .5
+        else:
+            token = "horse"
+            balance -= .5
+
+    # output
+    print(f"Token: {token}, Balance: ${balance:.2f}")
+
+print(f"Starting balance = ${STARTING_BALANCE:.2f}")
+print(f"Final balance = ${balance:.2f}")
+
