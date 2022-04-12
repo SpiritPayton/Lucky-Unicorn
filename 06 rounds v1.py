1e2d996a8bc15ce6817e1d43aad2f18531d6f536
Spirit Payton <98915494+SpiritPayton@users.noreply.github.com>
2022-04-13 10:09:28 +1200

    吃猪们

---
+++ 06 rounds v1.py
@@ -0,0 +1,13 @@
+"""Component 4 - game mechanics and looping v1
+Based on 05 token generator v4 but hard-wired to only allocate donkeys
+Gives user feedback about number of rounds and maintains balance
+Test amount set to $5
+"""
+
+import random
+
+# main routine
+TEST_AMOUNT = 5
+balance = TEST_AMOUNT
+
+rounds_played = 0
